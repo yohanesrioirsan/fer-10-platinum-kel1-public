84c3e6f8b49601dac281f8ac5190bc701fc8e8ea
Naufal Ariful <naufalariful16@gmail.com>
2023-06-11 10:53:44 +0700

    fixing importing component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,8 +5,8 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Offcanvas from "react-bootstrap/Offcanvas";
-import { RentButton } from "../../pages/Home/components/RentButton";
-import { SearchForm } from "../SearchForm";
+import RentButton  from "../../pages/Home/components/RentButton";
+import SearchForm  from "../SearchForm";
 
 function Hero(props) {
   const {
