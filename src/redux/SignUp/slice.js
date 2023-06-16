22069e03e63a847ae5647ce0a2bf09a71c6da031
yohanesrioirsan <rioirsan8@gmail.com>
2023-06-16 19:44:29 +0800

    fixing toast message

--- src/redux/SignUp/slice.js
+++ src/redux/SignUp/slice.js
@@ -34,6 +34,7 @@ const signupSlice = createSlice({
   },
 });
 
-export const { signupStart, signupSuccess, signupFailure } = signupSlice.actions;
+export const { signupStart, signupSuccess, signupFailure } =
+  signupSlice.actions;
 
 export default signupSlice.reducer;
